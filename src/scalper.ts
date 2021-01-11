7ad62a85f7409389f814a97c3a92e30ad2c2270d
armyaali <ali.umar.work@gmail.com>
2021-01-10 22:36:14 -0500

    script cleanup and finalization

--- src/scalper.ts
+++ src/scalper.ts
@@ -1,16 +1,16 @@
 import fs from "fs";
 import puppeteer from "puppeteer";
-
+import sendEmail from './email-service'
+import getTemplate from './config/emailTemplate';
 const FILE = "links.txt"
 let YOINKED = false;
-// const MY_CUSTOM_BROWSER_TO_USE = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
 
 const yoink = async (links: string[]) => {
     const BUTTON_SELECTOR = "button.addToCartButton";
     const DISABLED_BUTTON_SELECTOR = "button[disabled].addToCartButton";
-
+    const TITLE_SELECTOR = "h1.productName_19xJx";
     try {
-        const browser = await puppeteer.launch({ headless: false });
+        const browser = await puppeteer.launch({headless: false});
         // go to each link
         for (const link of links) {
             const page = await browser.newPage();
@@ -24,24 +24,35 @@ const yoink = async (links: string[]) => {
 
         while (!YOINKED) {
             for (const page of pages) {
+                await page.bringToFront();
                 await page.setDefaultNavigationTimeout(0);
+                await page.setDefaultTimeout(0);
+
                 const button = await page.$(BUTTON_SELECTOR);
                 const disabledButton = await page.$(DISABLED_BUTTON_SELECTOR);
+
                 if (button && !disabledButton) {
                     YOINKED = true;
                     FOUND_PAGE = page;
                     break;
                 }
-                await page.waitForTimeout(3000);
-                await page.reload({ waitUntil: ["networkidle2"] });
+                await page.waitForTimeout(1000);
+                await page.reload();
             }
         }
 
         if (YOINKED && FOUND_PAGE) {
-            const button = await FOUND_PAGE.$(BUTTON_SELECTOR);
-            if (button)
-                await button.click();
-            console.log(`yoinked ${FOUND_PAGE.url()}`);
+            const title = await FOUND_PAGE.evaluate(() => {
+                return document.querySelector("h1.productName_19xJx")?.textContent
+            });
+            await FOUND_PAGE.click(BUTTON_SELECTOR);
+            const titleContent = title as unknown as string;
+            if(titleContent) {
+                const date = new Date();
+                sendEmail(getTemplate({url: FOUND_PAGE.url(), itemName: titleContent, time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}));
+            }
+                
+           
         }
 
         await browser.close();
